31871bd99d96b952219cb265c34bf6b216e84f5a
Elexzandreia <elexzandreia@csu.fullerton.edu>
2023-12-02 17:29:07 -0800

    Integrated input.txt and got correct results

--- ExhaustiveSearch.py
+++ ExhaustiveSearch.py
@@ -6,47 +6,6 @@ class Stock:
         self.numberOfStocks = numberOfStocks
         self.cost = cost
 
-def stockOptimization(maxAmountAllowed, stocks):
-    currentBest = None
-    for i in range(len(stocks) + 1):
-        for candidateStocks in combinations(stocks, i):
-            if verifyCandidateStocks(maxAmountAllowed, candidateStocks):
-                if currentBest is None or totalNumberOfStocks(candidateStocks) > totalNumberOfStocks(currentBest): 
-                    currentBest = candidateStocks
-    return currentBest
-
-def verifyCandidateStocks(maxAmountAllowed, candidateStocks):
-    totalCostOfCandidateStocks = sum(stock.cost for stock in candidateStocks) 
-    if totalCostOfCandidateStocks <= maxAmountAllowed: 
-        return True
-    else:
-        return False
-
-def totalNumberOfStocks(candidateStocks):
-    totalStockNumber = sum(stock.numberOfStocks for stock in candidateStocks)
-    return totalStockNumber
-
-# Examples 
-stocks = [Stock(1, 2), Stock(4, 3), Stock(3, 6), Stock(6, 7)]
-maxCostAmount = 12
-#output should be 11; 1+4+6 at index 0,1,3, sum of the costs at these indices = 2+3+7<=12
-example1 = stockOptimization(maxCostAmount, stocks)
-result1 = 0
-print("First example:")
-for stock in example1:
-    result1 += stock.numberOfStocks
-print(result1)
-
-stocks2 = [Stock(3, 2), Stock(4, 3), Stock(5, 3), Stock(6, 7)]
-maxCostAmount2 = 10
-# output should be 12; 3+4+5 at index 0,1,2, sum of the costs at these indices = 2+3+3<=10
-example2 = stockOptimization(maxCostAmount2, stocks2)
-result2 = 0
-print("Second example:")
-for stock in example2:
-    result2 += stock.numberOfStocks
-print(result2)
-
 
 def readStockDataFromFile(fileName):
     allStocks = []
@@ -68,15 +27,43 @@ def readStockDataFromFile(fileName):
             allStocks.append(currentSetStocks) 
             maxAmounts.append(int(file.readline().strip()))
             file.readline()
+
     return allStocks, maxAmounts
 
-# Read stocks and max amounts from file
+def stockOptimization(maxAmountAllowed, stocks):
+    currentBest = None
+    for i in range(len(stocks) + 1):
+        for candidateStocks in combinations(stocks, i):
+            if verifyCandidateStocks(maxAmountAllowed, candidateStocks):
+                if currentBest is None or totalNumberOfStocks(candidateStocks) > totalNumberOfStocks(currentBest): 
+                    currentBest = candidateStocks
+    return currentBest
+
+def verifyCandidateStocks(maxAmountAllowed, candidateStocks):
+    totalCostOfCandidateStocks = sum(stock.cost for stock in candidateStocks) 
+    if totalCostOfCandidateStocks <= maxAmountAllowed: 
+        return True
+    else:
+        return False
+
+def totalNumberOfStocks(candidateStocks):
+    totalStockNumber = sum(stock.numberOfStocks for stock in candidateStocks)
+    return totalStockNumber
+
+
+
 allStocks, maxAmounts = readStockDataFromFile('input.txt')
 
-# Example to show the read stocks and max amounts
 for setIndex, stocks in enumerate(allStocks):
-    print(f"Set {setIndex + 1}:")
-    for stock in stocks:
-        print(f"  Number of Stocks: {stock.numberOfStocks}, Cost: {stock.cost}")
-    print(f"Max Amount for Set {setIndex + 1}: {maxAmounts[setIndex]}")
+    example1 = stockOptimization(maxAmounts[setIndex], stocks)
+    result1 = 0
     print()
+    for stock in example1:
+        print(f"Stocks selected: ({stock.numberOfStocks}, {stock.cost})")
+        result1 += stock.numberOfStocks
+    print(f"Max Amount for Set {setIndex + 1}: {maxAmounts[setIndex]}")
+    print(f"Result to be printed to output.txt: {result1}\n")
+
+# Examples 
+# For set 1, the output should be 11; 1+4+6 at index 0,1,3, sum of the costs at these indices = 2+3+7 = 11 (which is <= 12)
+# For set 2, the output should be 12; 3+4+5 at index 0,1,2, sum of the costs at these indices = 2+3+3 = 8 (which is <= 10)
